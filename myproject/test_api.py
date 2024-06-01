fb4d2dede7c86702224ced4a29bec61fa0db8e0c
nastyazz <nastyaz0808@mail.ru>
2024-06-01 21:04:42 +0300

    fixed tests

--- myproject/test_api.py
+++ myproject/test_api.py
@@ -1,76 +1,79 @@
 """Tests for the API routes."""
 import pytest
-import requests
+
+from app import app
 
 STATUS_CODE_SEARCH_MOVIE = 200
+UTF8ENCODING = 'utf-8'
 
 
 @pytest.fixture
-def api_url():
+def client():
     """
-    Fixture for the API URL.
+    Fixture for creating a test client.
 
-    Returns:
-        str: The base URL of the API.
+    Yields:
+        FlaskClient: The test client for the Flask application.
     """
-    return 'http://127.0.0.1:8080'
+    with app.test_client() as client:
+        yield client
 
 
-def test_index(api_url):
+def test_index(client):
     """
     Test the index route.
 
     Args:
-        api_url (str): The base URL of the API.
+        client (FlaskClient): The test client for the Flask application.
 
     Return:
         None
     """
-    response = requests.get(f'{api_url}/')
+    response = client.get('/')
     assert response.status_code == STATUS_CODE_SEARCH_MOVIE
-    assert 'Movie Search' in response.text
+    assert 'Movie Search' in response.data.decode(UTF8ENCODING)
 
 
-def test_search_movie(api_url):
+def test_search_movie(client):
     """
     Test the search_movie route.
 
     Args:
-        api_url (str): The base URL of the API.
+        client (FlaskClient): The test client for the Flask application.
 
     Return:
         None
     """
-    response = requests.get(f'{api_url}/search?query=черная любовь')
+    response = client.get('/search?query=черная любовь')
     assert response.status_code == STATUS_CODE_SEARCH_MOVIE
-    assert 'Черная любовь' in response.text
+    assert 'Черная любовь' in response.data.decode(UTF8ENCODING)
 
 
-def test_get_movie(api_url):
+def test_get_movie(client):
     """
     Test the get_movie route.
 
     Args:
-        api_url (str): The base URL of the API.
+        client (FlaskClient): The test client for the Flask application.
 
     Return:
         None
     """
-    response = requests.get(f'{api_url}/movie/266311')
+    response = client.get('/movie/266311')
     assert response.status_code == STATUS_CODE_SEARCH_MOVIE
-    assert 'Черная любовь' in response.text
+    assert 'Черная любовь' in response.data.decode(UTF8ENCODING)
 
 
-def test_get_actor(api_url):
+def test_get_actor(client):
     """
     Test the get_actor route.
 
     Args:
-        api_url (str): The base URL of the API.
+        client (FlaskClient): The test client for the Flask application.
 
     Return:
         None
     """
-    response = requests.get(f'{api_url}/actor/1455007')
+    response = client.get('/actor/1455007')
     assert response.status_code == STATUS_CODE_SEARCH_MOVIE
-    assert 'Бурак Озчивит' in response.text
+    assert 'Бурак Озчивит' in response.data.decode(UTF8ENCODING)
